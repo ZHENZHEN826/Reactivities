581552d2ede2763a85f02fec6d9dc87809faf93e
Tony <tony1995826@gmail.com>
2021-04-24 18:55:36 +1200

    Walking Skelton Part 1 - API

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public String Category { get; set; }
+        public String City { get; set; }
+    }
+}
